de978c0b6b345c464e45ab0519f34b3afc9d1858
grohalex <alexander.groh22@gmail.com>
2020-11-18 18:05:56 +0000

    Added a check for finding out whether the position is stuck, stuck_position(), and added a current measurement

--- Two-Way/current0.py
+++ Two-Way/current0.py
@@ -1,4 +1,4 @@
-# first version of two lattice lattice
+# first version of two way lattice for getting current
 import numpy as np
 import numpy.random as rd
 import random as random
@@ -24,6 +24,13 @@ steady_state = 2000 #10000    #after the transient phase
 #init
 L1 = np.zeros(N)    #initialize lattice 1
 L2 = np.zeros(N)    #initialize lattice 2
+#init the current measurement variables 'passed_particles'
+passed_particles1 = 0  #particles which passes 0th site in latice 1
+passed_particles2 = 0  #particles which passed 0th site in latice 2
+passed_particles1N = 0 #particles which passed Nth site in latice 1
+passed_particles2N = 0 #particles which passed Nth site in latice 2
+current1 = 0
+current2 = 1
 
 #update ftion
 def update(i):
@@ -31,25 +38,30 @@ def update(i):
     if i==0:
         if L1[0]==0 and rd.rand()<a1:
             L1[0]=1
+            passed_particles1 +=1
     #in case there site = 1 there is particle 2 which should leave
     elif i==1 and L1[0]==2 and rd.rand()<b2: #in case there is particle 2 it leaves
         L1[0]=0
-
+        passed_particles2N +=1
     #insertion a2
     elif i==N+1:
         if L2[0]==0 and rd.rand()<a1:
             L2[0]=2
+            passed_particles2 +=1
     #in case site = 2N+1 and there is particle 1 it leaves
     elif i==N+2 and L2[0]==1 and rd.rand()<b2: #in case there is particle 1 it leaves
         L2[0]=0
+        passed_particles1N +=1
     #removal b1
     elif i==N and rd.rand()<b1:
         if L1[-1]>0:
             L1[-1]=0
+            passed_particles1N +=1
     #removal b2
     elif i==2*N+1 and rd.rand()<b1:
         if L2[-1]>0:
             L2[-1]=0
+            passed_particles2N +=1
 
     #regular site lattice 1
     elif i>0 and i < N:
@@ -414,6 +426,7 @@ def stuck_position():
 ###########################################################################
 
 #Working simulation:
+'''
 j = 0
 while True:#j<100:
     print(j)
@@ -424,5 +437,18 @@ while True:#j<100:
     print(update_par(site, 1,1,1,1,1,1,1,1))
     print(stuck_position())
     DisplayNice()
-
     j+=1
+'''
+
+for i in range(steps):
+    for j in range(2*N+2):
+        site = rd.randint(0,2*N+2)
+        update(site)
+
+        #cutoff (steady state) period
+
+        current1 = passed_particles1/(steps-0)
+        current2 = passed_particles2/(steps-0)
+        current1N = passed_particles1N/(steps-0)
+        current2N = passed_particles2N/(steps-0)
+        DisplayNice()
